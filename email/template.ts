44867fca04ecd55f462e0711ab8255cbbf2ef2ce
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2025-01-14 17:13:21 +0530

    subscription form update

--- email/template.ts
+++ email/template.ts
@@ -39,3 +39,59 @@ export const emailTemplate = `<!DOCTYPE html>
     </div>
 </body>
 </html>`;
+
+export const subscriptionTemplate = `
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html dir="ltr" lang="en">
+  <head>
+    <meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
+    <meta name="x-apple-disable-message-reformatting" />
+    <!--$-->
+  </head>
+  <body
+    style='background-color:#ffffff;font-family:-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif'
+  >
+    <table
+      align="center"
+      width="100%"
+      border="0"
+      cellpadding="0"
+      cellspacing="0"
+      role="presentation"
+      style="max-width:37.5em;margin:0 auto;padding:20px 0 48px"
+    >
+      <tbody>
+        <tr style="width:100%">
+          <td>
+            <div style="display: flex; justify-content: center; align-items: center; margin: 20px 0;">
+            <svg width="100" height="100" viewBox="0 0 30 30">
+    <circle cx="10" cy="15" r="3" fill="#4F46E5"/>
+    <circle cx="20" cy="10" r="3" fill="#4F46E5"/>
+    <circle cx="20" cy="20" r="3" fill="#4F46E5"/>
+    <path d="M10 15 L20 10 M10 15 L20 20" stroke="#4F46E5" fill="none"/>
+</svg>
+            <span style="font-size: 50px; font-weight: bold; margin-left: 10px;">VISYNTRO</span>
+            </div>
+            <p style="font-size:16px;line-height:26px;margin:16px 0">
+              Hi
+              <!-- -->{name}<!-- -->,
+            </p>
+            <p style="font-size:16px;line-height:26px;margin:16px 0">
+              Welcome to Visyntro,an empowering business solutions with Expert Services.
+            </p>
+            <p style="font-size:16px;line-height:26px;margin:16px 0">
+              Best,<br />The Visyntro
+            </p>
+            <hr
+              style="width:100%;border:none;border-top:1px solid #eaeaea;border-color:#cccccc;margin:20px 0"
+            />
+      
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <!--/$-->
+  </body>
+</html>
+
+`;
---
+++ src/components/SubscriptionForm.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { toast } from "sonner";
+import registrationInput from "../../constant/registrationInput";
+import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Input } from "./ui/input";
+
+interface Props {
+  name: string;
+  className: string;
+}
+
+export const subscriptionForm = [
+  {
+    id: "name",
+    label: "Name",
+    type: "name",
+    required: true,
+  },
+  {
+    id: "email",
+    label: "Email Address",
+    type: "email",
+    required: true,
+  },
+];
+
+const SubscriptionForm = ({ name, className }: Props) => {
+  const [formData, setFormData] = useState({
+    email: "",
+    name: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setFormData((prev) => ({ ...prev, [id]: value }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("/api/subscription", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        toast.success("Subscribe successfully!");
+        setFormData({
+          email: "",
+          name: "",
+        });
+      } else {
+        toast.error("Failed to send email. Please try again.");
+      }
+    } catch (error) {
+      console.error("Error sending email:", error);
+      toast.error("Failed to send email. Please try again.");
+    }
+  };
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsOpen(true);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div>
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <DialogTrigger asChild>
+          <Button className={className}>{name}</Button>
+        </DialogTrigger>
+        <DialogContent className="p-4 w-[500px]">
+          <DialogHeader>
+            <DialogTitle className="text-center text-xl mb-2">
+              Subscribe Now
+            </DialogTitle>
+            <DialogDescription>
+              <form onSubmit={handleSubmit}>
+                {subscriptionForm.map(
+                  ({ id, label, type = "text", required }) => (
+                    <div key={id}>
+                      <label htmlFor={id} className="text-black">
+                        {label}
+                      </label>
+                      <Input
+                        type={type}
+                        id={id}
+                        placeholder={label}
+                        required={required}
+                        className="my-2"
+                        value={formData[id as keyof typeof formData]}
+                        onChange={handleChange}
+                      />
+                    </div>
+                  )
+                )}
+                <Button
+                  type="submit"
+                  className="w-full mt-5 bg-yellow-500 hover:bg-yellow-400 text-white font-semibold text-lg py-5 sm:py-6 px-6 sm:px-8 rounded-full"
+                >
+                  Submit
+                </Button>
+              </form>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default SubscriptionForm;
